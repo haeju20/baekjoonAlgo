f137959bd5f8c38b168bedf539f695ae66409752
jisu <1712wltn@gmail.com>
2022-08-24 14:30:14 +0900

    0824 백준 301 수학 1

---
+++ math1_301.py
@@ -0,0 +1,31 @@
+#9613
+import math
+t = int(input())
+for _ in range(t):
+    s = 0
+    num = list(map(int, input().split()))
+    for i in range(1, len(num)):
+        for j in range(i+1, len(num)):
+            s += math.gcd(num[i], num[j])
+    print(s)
+
+#17087
+import math
+n, s = map(int, input().split())
+a = list(map(int, input().split()))
+dis = list(set(abs(a[i] - s) for i in range(n)))
+d = min(dis)
+for i in range(len(dis)):
+    d = math.gcd(dis[i], d)
+print(d)
+
+#1373
+
+
+#1212
+
+
+#2089
+
+
+#17103
